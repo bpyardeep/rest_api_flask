3bbb08b07bd05c4b07a2c05a692d3aeb5f12622c
PYARDEEP BIRDI <b.pyardeep@gmail.com>
2022-03-23 22:53:40 +0530

    Initial Commit

---
+++ main.py
@@ -0,0 +1,20 @@
+from flask import Flask, jsonify, request
+  
+# creating a Flask app
+app = Flask(__name__)
+  
+# on the terminal type: curl http://127.0.0.1:5000/
+# returns hello world when we use GET.
+# returns the data that we send when we use POST.
+@app.route('/', methods = ['GET', 'POST'])
+def home():
+    if(request.method == 'GET'):
+  
+        data = "hello world"
+        return jsonify({'data': data})
+  
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
